4d4efa5038343b9e9a68b71f5e0c7916f84152f9
Dennis Heeke <dheeke1976@gmail.com>
2021-12-06 11:14:26 -0500

    Final

---
+++ Main.java
@@ -0,0 +1,64 @@
+package Word_Analyzer;
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override // Override the start method in the Application class
+	  public void start(Stage primaryStage) {
+	    // Text area for displaying contents
+	    TextArea ta = new TextArea();
+	    // Create a scene and place it in the stage
+	    Scene scene = new Scene(new ScrollPane(ta), 450, 200);
+	    primaryStage.setTitle("Server"); // Set the stage title
+	    primaryStage.setScene(scene); // Place the scene in the stage
+	    primaryStage.show(); // Display the stage
+	    
+	    new Thread( () -> {
+	      try {
+	        // Create a server socket
+	        ServerSocket serverSocket = new ServerSocket(8000);
+	        Platform.runLater(() ->
+	          ta.appendText("Server started at " + new Date() + "\n Standing by for number input\n"));
+	  
+	        // Listen for a connection request
+	        Socket socket = serverSocket.accept();
+	  
+	        // Create data input and output streams
+	        DataInputStream inputFromClient = new DataInputStream(
+	          socket.getInputStream());
+	        DataOutputStream outputToClient = new DataOutputStream(
+	          socket.getOutputStream());
+	  
+	        while (true) {
+	          // Receive input number from the client
+	          String inputNumber = inputFromClient.readUTF();
+	          System.out.println("Input website received is: " + inputNumber + "\n");
+	          ta.appendText("The website to be parsed is:"+inputNumber+"\n  Scanning text ...\n");
+	  
+	          // Send result back to the client
+	          outputToClient.writeUTF(inputNumber);
+	        }
+	      }
+	      catch(IOException ex) {
+	        ex.printStackTrace();
+	      }
+	    }).start();
+	    new Client().start(new Stage());
+ }
+	  /**
+	   * The main method is only needed for the IDE with limited
+	   * JavaFX support. Not needed for running from the command line.
+	   */
+	  public static void main(String[] args) {
+	    launch(args);
+	    
+	  }
+}
